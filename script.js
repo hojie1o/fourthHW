8ee3e80585310d03563d066d123fbbf224be3f14
hojiev1o <hojievamirbek@gmail.com>
2025-04-10 17:05:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let name = prompt("Ismingiz nima: ");
+let age = prompt("Yoshingiz nechida: ");
+let description = prompt("O'zingiz haqida qisqacha yozing: ");
+alert("Hello, my name is " + name + ", I am " + age + ", Here is a little description about me: " + description);
